5a9d60ea69e0ec91d39bbbd7dfc7a8e583932719
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-27 12:22:13 -0300

    081-Layout Login (básico).

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -23,6 +23,48 @@ class LoginPage extends StatelessWidget {
           ],
         ),
       ),
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          Text(
+            'Login',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          TextFormField(
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Senha',
+            style: TextStyle(color: Colors.lightBlueAccent),
+          ),
+          TextFormField(
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 46,
+            child: RaisedButton(
+              color: Colors.black.withAlpha(10),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
