d36d8f34d317e00544e613ba37f2233d79790d91
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-30 08:43:27 -0300

    112-TabBar: categorias.

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -5,13 +5,27 @@ import 'package:fluttercars/pages/carro/carros_listview.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Miniatura de Carros'),
-        centerTitle: true,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Miniatura de Carros'),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Clássicos'),
+              Tab(text: 'Esportivos'),
+              Tab(text: 'Luxo'),
+            ],
+          ),
+        ),
+        body: TabBarView(children: [
+          CarrosListView(),
+          CarrosListView(),
+          CarrosListView(),
+        ]),
+        drawer: DrawerList(),
       ),
-      body: CarrosListView(),
-      drawer: DrawerList(),
     );
   }
 }
