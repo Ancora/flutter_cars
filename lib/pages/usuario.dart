1ad745fe1acb812ff07812fc294c48c68c3f29eb
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-29 21:09:08 -0300

    095-Parser de JSON III (map roles).

--- lib/pages/usuario.dart
+++ lib/pages/usuario.dart
@@ -10,10 +10,32 @@ class Usuario {
       : login = map['login'],
         name = map['nome'],
         email = map['email'],
-        token = map['token'];
+        token = map['token'],
+        roles = map['roles'] != null
+            ? map['roles']
+                .map<String>(
+                  (role) => role.toString(),
+                )
+                .toList()
+            : null;
 
   @override
   String toString() {
-    return 'Usuario(login: $login, nome: $name, email: $email, token: $token, roles: roles)';
+    return 'Usuario(login: $login, nome: $name, email: $email, token: $token, roles: $roles)';
   }
+
+  // static List<String> getRoles(Map<String, dynamic> map) {
+  /* List list = map['roles'];
+    List<String> roles = list
+        .map<String>(
+          (role) => role.toString(),
+        )
+        .toList();
+    return roles; OU */
+  /* List<String> roles = [];
+    for (String role in list) {
+      roles.add(role);
+    }
+    return roles; */
+  // }
 }
