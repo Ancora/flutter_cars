20a6cf64adc3ec95dcd99dd57dd93941f3a6f5b6
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-30 18:29:17 -0300

    115-Shared Pref: TabBar - Listener.

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -10,29 +10,43 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin<HomePage> {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.index = 1;
+    _tabController.addListener(() {
+      print('Tab ${_tabController.index}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Miniatura de Carros'),
-          centerTitle: true,
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Clássicos'),
-              Tab(text: 'Esportivos'),
-              Tab(text: 'Luxo'),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Miniatura de Carros'),
+        centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Clássicos'),
+            Tab(text: 'Esportivos'),
+            Tab(text: 'Luxo'),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
           CarrosListView(TipoCarro.classicos),
           CarrosListView(TipoCarro.esportivos),
           CarrosListView(TipoCarro.luxo),
-        ]),
-        drawer: DrawerList(),
+        ],
       ),
+      drawer: DrawerList(),
     );
   }
 }
