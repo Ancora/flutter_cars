c5e3c708dc7dc523bed129c1acf1b5a0ba7e30b6
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-30 22:14:46 -0300

    122-SharedPrefers...-JWT /api/v2/carros.

--- lib/pages/carro/carros_api.dart
+++ lib/pages/carro/carros_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:fluttercars/pages/carro/carro.dart';
+import 'package:fluttercars/pages/login/usuario.dart';
 import 'package:http/http.dart' as http;
 
 /* enum TipoCarro { CLASSICOS, ESPORTIVOS, LUXO } */
@@ -12,21 +13,19 @@ class TipoCarro {
 
 class CarrosApi {
   static Future<List<Carro>> getCarros(String tipo) async {
-    /* String soTipo = tipo.toString().replaceAll('TipoCarro.', ''); */
+    Usuario user = await Usuario.get();
+
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${user.token}',
+    };
 
     var url =
-        'https://carros-springboot.herokuapp.com/api/v1/carros/tipo/$tipo';
+        'https://carros-springboot.herokuapp.com/api/v2/carros/tipo/$tipo';
     /* print('GET => $url'); */
-    var response = await http.get(url);
+    var response = await http.get(url, headers: headers);
 
     List list = jsonDecode(response.body);
-
     return list.map<Carro>((map) => Carro.fromJson(map)).toList();
-
-    /* for (Map map in list) {
-      Carro car = Carro.fromJson(map);
-      carros.add(car);
-    }
-    return carros;*/
   }
 }
