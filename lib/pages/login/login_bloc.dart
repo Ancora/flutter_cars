b721d417ea3227a7bd5341fc755287d672036615
Ancora <anderson.rocha@ancora-ti.com.br>
2020-03-31 15:24:00 -0300

    134-BLoC: simplificando o código-SimpleBloc().

--- lib/pages/login/login_bloc.dart
+++ lib/pages/login/login_bloc.dart
@@ -1,22 +1,15 @@
 import 'dart:async';
 
 import 'package:fluttercars/pages/api_response.dart';
+import 'package:fluttercars/pages/carro/simple_bloc.dart';
 import 'package:fluttercars/pages/login/login_api.dart';
 import 'package:fluttercars/pages/login/usuario.dart';
 
-class LoginBloc {
-  final _streamController = StreamController<bool>();
-
-  get stream => _streamController.stream;
-
+class LoginBloc extends SimpleBloc<bool> {
   Future<ApiResponse<Usuario>> login(String login, String password) async {
-    _streamController.add(true);
+    add(true);
     ApiResponse response = await LoginApi.login(login, password);
-    _streamController.add(false);
+    add(false);
     return response;
-  }
-
-  void dispose() {
-    _streamController.close();
   }
 }
