b9bf66af7ee4644136ab3c51354f815ec3e53bde
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-01 11:31:45 -0300

    137-MobX na prática.

--- lib/pages/login/login_bloc.dart
+++ lib/pages/login/login_bloc.dart
@@ -4,7 +4,7 @@ import 'package:fluttercars/pages/api_response.dart';
 import 'package:fluttercars/pages/carro/simple_bloc.dart';
 import 'package:fluttercars/pages/login/login_api.dart';
 import 'package:fluttercars/pages/login/usuario.dart';
-
+ 
 class LoginBloc extends SimpleBloc<bool> {
   Future<ApiResponse<Usuario>> login(String login, String password) async {
     add(true);
