ea9ce651c5a4aa2305b19dc3a44aa0c99be5a31b
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-03 11:31:40 -0300

    157-0Cache com BD: plugin connectivity (cont.).

--- lib/pages/carros/carros_bloc.dart
+++ lib/pages/carros/carros_bloc.dart
@@ -1,10 +1,19 @@
 import 'package:fluttercars/pages/carros/carro.dart';
 import 'package:fluttercars/pages/carros/carros_api.dart';
 import 'package:fluttercars/pages/carros/simple_bloc.dart';
+import 'package:fluttercars/pages/favoritos/carro_dao.dart';
+import 'package:fluttercars/utils/network.dart';
 
 class CarrosBloc extends SimpleBloc<List<Carro>> {
   Future<List<Carro>> fetch(String tipo) async {
     try {
+      bool networkOn = await isNetworkOn();
+      if (!networkOn) {
+        List<Carro> carros = await CarroDAO().findAllByTipo(tipo);
+        add(carros);
+        return carros;
+      }
+
       List<Carro> carros = await CarrosApi.getCarros(tipo);
       add(carros);
       return carros;
