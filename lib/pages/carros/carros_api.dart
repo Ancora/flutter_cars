d749c2a5d7c3c3f9129b6ae7c387745baaa2d980
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-06 16:36:16 -0300

    175-HTTP Header e Token JWT.

--- lib/pages/carros/carros_api.dart
+++ lib/pages/carros/carros_api.dart
@@ -2,8 +2,7 @@ import 'dart:convert' as convert;
 
 import 'package:fluttercars/pages/api_response.dart';
 import 'package:fluttercars/pages/carros/carro.dart';
-import 'package:fluttercars/pages/login/usuario.dart';
-import 'package:http/http.dart' as http;
+import 'package:fluttercars/utils/http_helper.dart' as http;
 
 /* enum TipoCarro { CLASSICOS, ESPORTIVOS, LUXO } */
 class TipoCarro {
@@ -14,17 +13,9 @@ class TipoCarro {
 
 class CarrosApi {
   static Future<List<Carro>> getCarros(String tipo) async {
-    Usuario user = await Usuario.get();
-
-    Map<String, String> headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ${user.token}',
-    };
-
     var url =
         'https://carros-springboot.herokuapp.com/api/v2/carros/tipo/$tipo';
-    var response = await http.get(url, headers: headers);
-    print(url);
+    var response = await http.get(url);
 
     List list = convert.jsonDecode(response.body);
     List<Carro> carros = list.map<Carro>((map) => Carro.fromMap(map)).toList();
@@ -34,26 +25,15 @@ class CarrosApi {
 
   static Future<ApiResponse<bool>> save(Carro car) async {
     try {
-      Usuario user = await Usuario.get();
-
-      Map<String, String> headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${user.token}',
-      };
       var url = 'https://carros-springboot.herokuapp.com/api/v2/carros';
       if (car.id != null) {
         url += '/${car.id}';
       }
 
-      print('POST >> $url');
-
       String json = car.toJson();
       var response = await (car.id == null
-          ? http.post(url, body: json, headers: headers)
-          : http.put(url, body: json, headers: headers));
-
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
+          ? http.post(url, body: json)
+          : http.put(url, body: json));
 
       if (response.statusCode == 200 || response.statusCode == 201) {
         Map mapResponse = convert.jsonDecode(response.body);
@@ -75,16 +55,10 @@ class CarrosApi {
 
   static Future<ApiResponse<bool>> delete(Carro car) async {
     try {
-      Usuario user = await Usuario.get();
-
-      Map<String, String> headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${user.token}',
-      };
       var url =
           'https://carros-springboot.herokuapp.com/api/v2/carros/${car.id}';
 
-      var response = await http.delete(url, headers: headers);
+      var response = await http.delete(url);
 
       if (response.statusCode == 200) {
         return ApiResponse.ok(true);
