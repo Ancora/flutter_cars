445447a9646c78e99424741c7f22603ad48a835e
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-10 13:06:00 -0300

    212-Firebase: login com email e senha.

--- lib/firebase/firebase_service.dart
+++ lib/firebase/firebase_service.dart
@@ -7,12 +7,39 @@ class FirebaseService {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<ApiResponse> login(String email, String password) async {
+    try {
+      // Login no Firebase
+      AuthResult result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      final FirebaseUser fuser = result.user;
+      print("Firebase Nome: ${fuser.displayName}");
+      print("Firebase Email: ${fuser.email}");
+      print("Firebase Foto: ${fuser.photoUrl}");
+
+      // Cria um usuario do app
+      final user = Usuario(
+        nome: fuser.displayName,
+        login: fuser.email,
+        email: fuser.email,
+        urlFoto: fuser.photoUrl,
+      );
+      user.save();
+
+      // Resposta genérica
+      return ApiResponse.ok();
+    } catch (error) {
+      print("Firebase error $error");
+      return ApiResponse.error(msg: 'Não foi possível acessar!');
+    }
+  }
+
   Future<ApiResponse> loginGoogle() async {
     try {
       // Login com o Google
       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
       final GoogleSignInAuthentication googleAuth =
-      await googleUser.authentication;
+          await googleUser.authentication;
 
       print("Google User: ${googleUser.email}");
 
@@ -25,9 +52,9 @@ class FirebaseService {
       // Login no Firebase
       AuthResult result = await _auth.signInWithCredential(credential);
       final FirebaseUser fuser = result.user;
-      print("Firebase Nome: " + fuser.displayName);
-      print("Firebase Email: " + fuser.email);
-      print("Firebase Foto: " + fuser.photoUrl);
+      print("Firebase Nome: ${fuser.displayName}");
+      print("Firebase Email: ${fuser.email}");
+      print("Firebase Foto: ${fuser.photoUrl}");
 
       // Cria um usuario do app
       final user = Usuario(
--- lib/pages/login/login_bloc.dart
+++ lib/pages/login/login_bloc.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 
+import 'package:fluttercars/firebase/firebase_service.dart';
 import 'package:fluttercars/pages/api_response.dart';
 import 'package:fluttercars/pages/carros/simple_bloc.dart';
-import 'package:fluttercars/pages/login/login_api.dart';
+//import 'package:fluttercars/pages/login/login_api.dart';
 import 'package:fluttercars/pages/login/usuario.dart';
 
 class LoginBloc extends SimpleBloc<bool> {
-  Future<ApiResponse<Usuario>> login(String login, String password) async {
+  //Future<ApiResponse<Usuario>> login(String login, String password) async {
+  Future<ApiResponse> login(String login, String password) async {
     add(true);
-    ApiResponse response = await LoginApi.login(login, password);
+    //ApiResponse response = await LoginApi.login(login, password);
+    ApiResponse response = await FirebaseService().login(login, password);
     add(false);
     return response;
   }
