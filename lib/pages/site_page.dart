509d2819c0673ee1283a40901fe21f51d8e4dcfd
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-13 21:43:01 -0300

    229-WebView c/ onPageFinished e IndexedStack.

--- lib/pages/site_page.dart
+++ lib/pages/site_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class SitePage extends StatelessWidget {
+class SitePage extends StatefulWidget {
+  @override
+  _SitePageState createState() => _SitePageState();
+}
+
+class _SitePageState extends State<SitePage> {
   WebViewController controller;
+  var _stackIdx = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -16,31 +22,62 @@ class SitePage extends StatelessWidget {
           ],
         ),
       ),
-      padding: const EdgeInsets.all(16),
+      //padding: const EdgeInsets.all(16),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Site Oficial'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: _onClickRefresh,
-            )
-          ],
-        ),
-        body: WebView(
-          initialUrl: 'https://ancora.eti.br',
-          onWebViewCreated: (controller) {
-            this.controller = controller;
-          },
-          navigationDelegate: (request) {
-            return NavigationDecision.navigate;
-          },
-        ),
-      ),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Site Oficial'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: _onClickRefresh,
+              )
+            ],
+          ),
+          body: _webView()),
     );
   }
 
   void _onClickRefresh() {
     this.controller.reload();
+  }
+
+  _webView() {
+    return IndexedStack(
+      index: _stackIdx,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Expanded(
+              child: WebView(
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: 'https://ancora.eti.br',
+                onPageFinished: _onPageFinished,
+              ),
+            ),
+          ],
+        ),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 0, 100),
+                Color.fromARGB(255, 0, 0, 150),
+                Color.fromARGB(255, 0, 0, 100),
+              ],
+            ),
+          ),
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onPageFinished(String url) {
+    setState(() {
+      _stackIdx = 0;
+    });
   }
 }
